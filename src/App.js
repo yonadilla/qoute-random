3cabc1732962a3264cf7bec26a0e2b33400934b4
unknown <yonadi322@gmail.com>
2023-04-02 04:59:49 +0700

    first deploy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { library } from '@fortawesome/fontawesome-svg-core';
 
+
+library.add(faTwitter)
 function App() {
+  const [quote, setQuote] = useState({text: "", author: ""});
+
+  useEffect(() => {
+      fetchQuote();
+  }, [])
+  
+  const fetchQuote = () => {
+    axios
+          .get("https://type.fit/api/quotes")
+          .then((response) =>{
+            const data = response.data;
+            const randomIndex = Math.floor(Math.random() * data.length);
+            const randomQuote = data[randomIndex]
+            const text = randomQuote.text;
+            const author = randomQuote.author;
+            setQuote({text, author});
+            console.log(randomQuote.author) 
+          })  
+          .catch((error) => {
+            console.log(error);
+          });
+          
+  };
+
+    const tweetQuote = () => {
+      const twitterUrl = `https://twitter.com/intent/tweet?text=${quote.text} - ${quote.author}`;
+      window.open (twitterUrl, '_blank');
+    }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="quote-box">
+        <h1 id="text">" {quote.text} "</h1>
+        <p id="author"> - {quote.author}</p>
+        <div id="button">
+        <a id="tweet-quote" href = {`https://twitter.com/intent/tweet?te  xt=${quote.text} - ${quote.author}`} target = '_blank' ><FontAwesomeIcon icon={faTwitter} /></a>
+        <button id="new-quote"onClick={fetchQuote}>next</button>        
+        </div>
     </div>
+    
   );
 }
 
